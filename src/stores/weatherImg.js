8bfc2e04588479e99bf8c3ea331a3c9b47367721
Dinieva Rezeda <dinievarezeda@mail.ru>
2024-03-23 01:22:14 +0300

    Change img path in weatherImg.js delete /public

--- src/stores/weatherImg.js
+++ src/stores/weatherImg.js
@@ -4,24 +4,24 @@ export const useWeatherImgStore = defineStore('weatherImg', () => {
   
     const weatherSvg = [
       { title: 'ясно', path: '/forecast/weather-svg/sun.svg' },
-      { title: 'небольшая облачность', path: '/forecast/public/weather-svg/sun-cloud.svg' },
-      { title: 'переменная облачность', path: '/forecast/public/weather-svg/sun-cloud.svg' },
+      { title: 'небольшая облачность', path: '/forecast/weather-svg/sun-cloud.svg' },
+      { title: 'переменная облачность', path: '/forecast/weather-svg/sun-cloud.svg' },
       {
         title: 'облачно с прояснениями',
-        path: '/forecast/public/weather-svg/sun-cloud.svg'
+        path: '/forecast/weather-svg/sun-cloud.svg'
       },
-      { title: 'облачно', path: '/forecast/public/weather-svg/cloudy.svg' },
-      { title: 'пасмурно', path: '/forecast/public/weather-svg/cloudy2.svg' },
+      { title: 'облачно', path: '/forecast/weather-svg/cloudy.svg' },
+      { title: 'пасмурно', path: '/forecast/weather-svg/cloudy2.svg' },
     
-      { title: 'снег', path: '/forecast/public/weather-svg/snowing.svg' },
-      { title: 'небольшой снег', path: '/forecast/public/weather-svg/light-snow.svg' },
-      { title: 'небольшой снегопад', path: '/forecast/public/weather-svg/light-snow.svg' },
-      { title: 'небольшой дождь', path: '/forecast/public/weather-svg/rain.svg' },
-      { title: 'морось', path: '/forecast/public/weather-svg/rain.svg' },
-      { title: 'дождь', path: '/forecast/public/weather-svg/rain.svg' },
-      { title: 'ливень', path: '/forecast/public/weather-svg/rain1.svg' },
-      { title: 'мгла', path: '/forecast/public/weather-svg/fog.svg' },
-      { title: 'туман', path: '/forecast/public/weather-svg/fog.svg' }
+      { title: 'снег', path: '/forecast/weather-svg/snowing.svg' },
+      { title: 'небольшой снег', path: '/forecast/weather-svg/light-snow.svg' },
+      { title: 'небольшой снегопад', path: '/forecast/weather-svg/light-snow.svg' },
+      { title: 'небольшой дождь', path: '/forecast/weather-svg/rain.svg' },
+      { title: 'морось', path: '/forecast/weather-svg/rain.svg' },
+      { title: 'дождь', path: '/forecast/weather-svg/rain.svg' },
+      { title: 'ливень', path: '/forecast/weather-svg/rain1.svg' },
+      { title: 'мгла', path: '/forecast/weather-svg/fog.svg' },
+      { title: 'туман', path: '/forecast/weather-svg/fog.svg' }
       ]
     return {
         weatherSvg
